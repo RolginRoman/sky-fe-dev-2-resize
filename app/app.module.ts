8f91ba05d3ad381465e3affdffd0a6148a767d29
RolginRoman <irolgin@gmail.com>
2020-09-02 10:16:31 +0400

    move to module

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
-import { HelloComponent } from "./hello.component";
 import { TestComponent } from "./test.component";
-import { ViewportModule } from "./viewport.module";
-import { ViewportConfigToken, IConfig } from "./viewport.config";
+import { ViewportModule } from "./viewport/viewport.module";
+import { ViewportConfigToken, IConfig } from "./viewport/viewport.config";
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, ViewportModule],
-  declarations: [AppComponent, HelloComponent, TestComponent],
+  imports: [BrowserModule, ViewportModule],
+  declarations: [AppComponent, TestComponent],
   bootstrap: [AppComponent],
   providers: [
     {
---
+++ app/viewport/viewport-resize.service.ts
@@ -0,0 +1,33 @@
+import { Injectable, Inject } from "@angular/core";
+import { DOCUMENT } from "@angular/common";
+import { defer, fromEvent } from "rxjs";
+import { map, throttleTime, startWith, shareReplay } from "rxjs/operators";
+
+// const fromRAF = (): Observable<void> => {
+// return (interval(10, animationFrameScheduler) as unknown) as Observable<void>;
+// };
+
+interface ViewportSizeEvent {
+  readonly width: number;
+}
+
+@Injectable()
+export class ViewportResizeService {
+  constructor(@Inject(DOCUMENT) private doc: Document) {}
+
+  public resize$ = defer(() => {
+    return fromEvent(this.doc.defaultView, "resize").pipe(
+      map(() => this.getCurrentViewportSizeEvent()),
+      startWith(this.getCurrentViewportSizeEvent()),
+      throttleTime(300) // it is possible to throttle by raf scheduler
+    );
+  }).pipe(shareReplay(1));
+
+  private getCurrentViewportSizeEvent(): ViewportSizeEvent {
+    return { width: this.getCurrentViewportWidth() };
+  }
+
+  private getCurrentViewportWidth(): number {
+    return this.doc.defaultView ? this.doc.defaultView.innerWidth : undefined;
+  }
+}
